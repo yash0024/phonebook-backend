01345775afc2ddfb987324874dfebd1b7d73ad73
Yash Ramani <yash.ramani@mail.utoronto.ca>
2022-05-09 08:10:59 -0400

    AddingBaseCode

---
+++ index.js
@@ -0,0 +1,126 @@
+const { request } = require('express');
+const express = require('express')
+const app = express()
+app.use(express.json())
+const morgan = require('morgan')
+app.use(morgan('tiny'));
+
+morgan.token('postData', (request) => {
+    if (request.method == 'POST') {
+        return JSON.stringify(request.body)
+    }
+    else {
+        return ' '
+    }
+})
+
+app.use(morgan(
+    ':method :url :status :res[content-length] - :response-time ms :postData'
+))
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+  })
+  
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+  })
+
+app.get('/info', (request, response) => {
+    response.send(`
+        <div>
+            <p>
+                Phonebook has info for ${persons.length} people
+            </p>
+            <p>
+                ${new Date()}
+            </p>
+        </div>`
+    )
+  })
+
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+  
+    response.status(204).end()
+  })
+
+const generateId = () => {
+    const maxId = persons.length > 0
+      ? Math.max(...persons.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
+  
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+  
+    if (!body.name) {
+      return response.status(400).json({ 
+        error: 'name missing' 
+      })
+    }
+
+    if (!body.number) {
+        return response.status(400).json({ 
+          error: 'number missing' 
+        })
+      }
+    
+    if (persons.map(person => person.name).includes(body.name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const person = {
+      name: body.name,
+      number: body.number,
+      id: generateId(),
+    }
+  
+    persons = persons.concat(person)
+  
+    response.json(person)
+  })
+
+const PORT = process.env.PORT || 3003
+app.listen(PORT, () => {
+console.log(`Server running on port ${PORT}`)
+})
